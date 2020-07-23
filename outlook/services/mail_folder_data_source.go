c16796d70f69760c0b3a2d798f809bb312c92f63
magodo <wztdyl@sina.com>
2020-07-23 21:53:10 +0800

    Data Source `outlook_mail_folder`: support `well_known_name`

--- outlook/services/mail_folder_data_source.go
+++ outlook/services/mail_folder_data_source.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/magodo/terraform-provider-outlook/outlook/clients"
+	"github.com/magodo/terraform-provider-outlook/outlook/validation"
 	msgraph "github.com/yaegashi/msgraph.go/v1.0"
 )
 
@@ -22,12 +23,40 @@ func DataSourceMailFolder() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Optional:     true,
+				ExactlyOneOf: []string{"name", "well_known_name"},
 			},
 			"parent_folder_id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ConflictsWith: []string{"well_known_name"},
+			},
+			"well_known_name": {
 				Type:     schema.TypeString,
 				Optional: true,
+				ValidateDiagFunc: validation.StringInSlice([]string{
+					"archive",
+					"clutter",
+					"conflicts",
+					"conversationhistory",
+					"deleteditems",
+					"drafts",
+					"inbox",
+					"junkemail",
+					"localfailures",
+					"msgfolderroot",
+					"outbox",
+					"recoverableitemsdeletions",
+					"scheduled",
+					"searchfolders",
+					"sentitems",
+					"severfailures",
+					"syncissues",
+				},
+					false),
+				ExactlyOneOf:  []string{"name", "well_known_name"},
+				ConflictsWith: []string{"parent_folder_id"},
 			},
 		},
 	}
@@ -37,32 +66,41 @@ func dataSourceMailRead(ctx context.Context, d *schema.ResourceData, meta interf
 	client := meta.(*clients.Client).MailFolders
 
 	name := d.Get("name").(string)
+	wellKnownName := d.Get("well_known_name").(string)
 	parent := d.Get("parent_folder_id").(string)
 
-	var (
-		objs []msgraph.MailFolder
-		err  error
-	)
-	if parent == "" {
-		req := client.Request()
-		req.Filter(fmt.Sprintf(`displayName eq '%s'`, name))
-		objs, err = req.Get(ctx)
+	var obj *msgraph.MailFolder
+	if wellKnownName != "" {
+		var err error
+		obj, err = client.ID(wellKnownName).Request().Get(ctx)
+		if err != nil {
+			return diag.FromErr(err)
+		}
 	} else {
-		req := client.ID(parent).ChildFolders().Request()
-		req.Filter(fmt.Sprintf(`displayName eq '%s'`, name))
-		objs, err = req.Get(ctx)
+		var (
+			objs []msgraph.MailFolder
+			err  error
+		)
+		if parent == "" {
+			req := client.Request()
+			req.Filter(fmt.Sprintf(`displayName eq '%s'`, name))
+			objs, err = req.Get(ctx)
+		} else {
+			req := client.ID(parent).ChildFolders().Request()
+			req.Filter(fmt.Sprintf(`displayName eq '%s'`, name))
+			objs, err = req.Get(ctx)
+		}
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		if len(objs) != 1 {
+			return diag.Errorf("expect one mail folder but got %d", len(objs))
+		}
+		obj = &objs[0]
 	}
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	if len(objs) != 1 {
-		return diag.Errorf("expect one mail folder but got %d", len(objs))
-	}
-	obj := objs[0]
 	if obj.ID == nil || *obj.ID == "" {
 		return diag.Errorf("empty or nil ID returned for Mail Folder %q ID", name)
 	}
-
 	d.SetId(*obj.ID)
 
 	return nil
